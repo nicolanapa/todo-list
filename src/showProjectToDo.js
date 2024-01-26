6f7da80221604cf1b6f7e1de6798d65e58533860
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-01-26 21:45:19 +0100

    Added some comments to express the idea and reminders in showProjectToDo.js

--- src/showProjectToDo.js
+++ src/showProjectToDo.js
@@ -1,5 +1,5 @@
-// Shows the default project
-// And the others
+// FIRST Shows the default project
+// SECOND And the other projects (one function about FIRST another about SECOND)
 /*  If project == no todos then only show title of project and add some space
     for future todos
 */
@@ -7,9 +7,18 @@
 /* When creating new project, append them to the bottom (appends to a 
 future new "main container")
 */ 
-
-function showDefaultProject() {
+/*
+    For each todos view only title and duedate
+    Make a little button / click the todo
+    that opens (dialog like in form) the entire todo with an close button
+    Put near the little view button a delete button and a edit details button
     
+    For the edit details button just make it similar to the toDo creating function
+    but only make available changing title, description, notes, priority and duedate
+    not the project where the todo is (for now)
+*/
+function showDefaultProject() {
+
 }
 
 export { showDefaultProject };
