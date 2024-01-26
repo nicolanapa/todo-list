6347d9feb04438d7dbd387e97d0c172226d93cd2
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-01-26 20:08:54 +0100

    Changed

--- src/formToDo.js
+++ src/formToDo.js
@@ -21,65 +21,80 @@ function dateChecker() { // Creates an variable with today's date
     return todayDate;
 }
 
-
-
-
 function addFormDOM(projectNameArray) {
-    let formContainer = document.querySelector("#form-container");
-
     /* cose che mancano
     //this.checklist = checklist;
     */
+   // POPUP
+    function dialogForm() {
+        let formContainer = document.querySelector("#form-container");
 
-    let dialogFormContainer = document.createElement("dialog");
-    dialogFormContainer.setAttribute("id", "dialog-form");
-    let formStesso = document.createElement("form");
-    formStesso.setAttribute("method", "dialog");
-    formStesso.setAttribute("action", "#");
-    formContainer.appendChild(dialogFormContainer);
-    dialogFormContainer.appendChild(formStesso);
+        
+
+        let dialogFormContainer = document.createElement("dialog");
+        dialogFormContainer.setAttribute("id", "dialog-form");
+        let formStesso = document.createElement("form");
+        formStesso.setAttribute("method", "dialog");
+        formStesso.setAttribute("action", "#");
+        formContainer.appendChild(dialogFormContainer);
+        dialogFormContainer.appendChild(formStesso);
+    }
+    
+    dialogForm();
 
     // TITOLO
-    let titleLabel = document.createElement("label");
-    titleLabel.textContent = "Titolo ToDo:"
-    let titleInput = document.createElement("input");
-    titleLabel.setAttribute("for", "title");
-    titleInput.setAttribute("type", "text");
-    titleInput.setAttribute("id", "title");
-    titleInput.setAttribute("name", "title");
-    titleInput.setAttribute("placeholder", "Working");
-    titleInput.setAttribute("value", "Working");//DA CANCELLARE
+    function titleForm() {
+        let titleLabel = document.createElement("label");
+        titleLabel.textContent = "Titolo ToDo:"
+        let titleInput = document.createElement("input");
+        titleLabel.setAttribute("for", "title");
+        titleInput.setAttribute("type", "text");
+        titleInput.setAttribute("id", "title");
+        titleInput.setAttribute("name", "title");
+        titleInput.setAttribute("placeholder", "Working");
+        titleInput.setAttribute("value", "Working");//DA CANCELLARE
 
-    formStesso.appendChild(titleLabel);
-    formStesso.appendChild(titleInput);
+        formStesso.appendChild(titleLabel);
+        formStesso.appendChild(titleInput);
+    }
+
+    titleForm();
 
     // DESCRIZIONE
-    let descriptionLabel = document.createElement("label");
-    descriptionLabel.textContent = "Descrizione generale del ToDo:"
-    let descriptionInput = document.createElement("input");
-    descriptionLabel.setAttribute("for", "description");
-    descriptionInput.setAttribute("type", "text");
-    descriptionInput.setAttribute("id", "description");
-    descriptionInput.setAttribute("name", "description");
-    descriptionInput.setAttribute("placeholder", "Go to work on x and y");
-    descriptionInput.setAttribute("value", "Go to work on x and y");//DA CANCELLARE
+    function descriptionForm() {
+        let descriptionLabel = document.createElement("label");
+        descriptionLabel.textContent = "Descrizione generale del ToDo:"
+        let descriptionInput = document.createElement("input");
+        descriptionLabel.setAttribute("for", "description");
+        descriptionInput.setAttribute("type", "text");
+        descriptionInput.setAttribute("id", "description");
+        descriptionInput.setAttribute("name", "description");
+        descriptionInput.setAttribute("placeholder", "Go to work on x and y");
+        descriptionInput.setAttribute("value", "Go to work on x and y");//DA CANCELLARE
 
-    formStesso.appendChild(descriptionLabel);
-    formStesso.appendChild(descriptionInput);
+        formStesso.appendChild(descriptionLabel);
+        formStesso.appendChild(descriptionInput);
+    }
+    
+    descriptionForm();
 
     // NOTE
-    let notesLabel = document.createElement("label");
-    notesLabel.textContent = "Note aggiuntive del ToDo:"
-    let notesInput = document.createElement("input");
-    notesLabel.setAttribute("for", "notes");
-    notesInput.setAttribute("type", "text");
-    notesInput.setAttribute("id", "notes");
-    notesInput.setAttribute("name", "notes");
-    notesInput.setAttribute("placeholder", "Don't forget z");
-    notesInput.setAttribute("value", "Don't forget z");//DA CANCELLARE
+    function notesForm() {
+        let notesLabel = document.createElement("label");
+        notesLabel.textContent = "Note aggiuntive del ToDo:"
+        let notesInput = document.createElement("input");
+        notesLabel.setAttribute("for", "notes");
+        notesInput.setAttribute("type", "text");
+        notesInput.setAttribute("id", "notes");
+        notesInput.setAttribute("name", "notes");
+        notesInput.setAttribute("placeholder", "Don't forget z");
+        notesInput.setAttribute("value", "Don't forget z");//DA CANCELLARE
 
-    formStesso.appendChild(notesLabel);
-    formStesso.appendChild(notesInput);
+        formStesso.appendChild(notesLabel);
+        formStesso.appendChild(notesInput);
+    }
+    
+    notesForm();
 
     // CHECKLIST | checkbox
     /*let checkListLabel = document.createElement("label");
@@ -96,67 +111,83 @@ function addFormDOM(projectNameArray) {
     // eventListener for creating new "cheklists"
 
     // DUEDATE | date
-    let dueDateLabel = document.createElement("label");
-    dueDateLabel.textContent = "ToDo da completare entro:"
-    let dueDateInput = document.createElement("input");
-    dueDateLabel.setAttribute("for", "dueDate");
-    dueDateInput.setAttribute("type", "date");
-    dueDateInput.setAttribute("min", "2000-01-01")
-    dueDateInput.setAttribute("max", "2099-12-31");
-    dueDateInput.setAttribute("id", "dueDate");
-    dueDateInput.setAttribute("name", "dueDate");
-    dueDateInput.setAttribute("value", dateChecker());//DEFAULT: TODAY
+    function dueDateForm() {
+        let dueDateLabel = document.createElement("label");
+        dueDateLabel.textContent = "ToDo da completare entro:"
+        let dueDateInput = document.createElement("input");
+        dueDateLabel.setAttribute("for", "dueDate");
+        dueDateInput.setAttribute("type", "date");
+        dueDateInput.setAttribute("min", "2000-01-01")
+        dueDateInput.setAttribute("max", "2099-12-31");
+        dueDateInput.setAttribute("id", "dueDate");
+        dueDateInput.setAttribute("name", "dueDate");
+        dueDateInput.setAttribute("value", dateChecker());//DEFAULT: TODAY
 
-    formStesso.appendChild(dueDateLabel);
-    formStesso.appendChild(dueDateInput);
+        formStesso.appendChild(dueDateLabel);
+        formStesso.appendChild(dueDateInput);
+    }
+    
+    dueDateForm();
 
     // PRIORITY
-    let priorityLabel = document.createElement("label");
-    priorityLabel.textContent = "Priorità ToDo (1 a 5) (1 = più priorità, 5 = meno priorità):"
-    let priorityInput = document.createElement("input");
-    priorityLabel.setAttribute("for", "priority");
-    priorityInput.setAttribute("type", "range");
-    priorityInput.setAttribute("min", "1");
-    priorityInput.setAttribute("max", "5");
-    priorityInput.setAttribute("id", "priority");
-    priorityInput.setAttribute("name", "priority");
-    priorityInput.setAttribute("placeholder", "1/2/3/4/5");
-    priorityInput.setAttribute("value", "1");//DA CANCELLARE, forse
+    function priorityForm() {
+        let priorityLabel = document.createElement("label");
+        priorityLabel.textContent = "Priorità ToDo (1 a 5) (1 = più priorità, 5 = meno priorità):"
+        let priorityInput = document.createElement("input");
+        priorityLabel.setAttribute("for", "priority");
+        priorityInput.setAttribute("type", "range");
+        priorityInput.setAttribute("min", "1");
+        priorityInput.setAttribute("max", "5");
+        priorityInput.setAttribute("id", "priority");
+        priorityInput.setAttribute("name", "priority");
+        priorityInput.setAttribute("placeholder", "1/2/3/4/5");
+        priorityInput.setAttribute("value", "1");//DA CANCELLARE, forse
 
-    formStesso.appendChild(priorityLabel);
-    formStesso.appendChild(priorityInput);
+        formStesso.appendChild(priorityLabel);
+        formStesso.appendChild(priorityInput);
+    }
+    
+    priorityForm();
 
     // PRIORITY Input color
-    priorityInput.addEventListener("input", () => { 
-    if ( priorityInput.value == 1 ) {
-        priorityInput.style.accentColor = "blue";
-    }
-    else if ( priorityInput.value == 2 ) {
-        priorityInput.style.accentColor = "#6495ed";
+    function priorityInputColorForm() {
+        priorityInput.addEventListener("input", () => { 
+        if ( priorityInput.value == 1 ) {
+            priorityInput.style.accentColor = "blue";
+        }
+        else if ( priorityInput.value == 2 ) {
+            priorityInput.style.accentColor = "#6495ed";
 
-    }    
-    else if ( priorityInput.value == 3 ) {
-        priorityInput.style.accentColor = "red";
+        }    
+        else if ( priorityInput.value == 3 ) {
+            priorityInput.style.accentColor = "red";
 
+        }
+        else if ( priorityInput.value == 4 ) {
+            priorityInput.style.accentColor = "#D7A6A7";
+        }
+        else if ( priorityInput.value == 5 ) {
+            priorityInput.style.accentColor = "#FFF9C6";
+        }
+        });
     }
-    else if ( priorityInput.value == 4 ) {
-        priorityInput.style.accentColor = "#D7A6A7";
-    }
-    else if ( priorityInput.value == 5 ) {
-        priorityInput.style.accentColor = "#FFF9C6";
-    }
-    });
+    
+    priorityInputColorForm();
 
     // PROJECTNAME Select creation
-    let projectNameLabel = document.createElement("label");
-    projectNameLabel.textContent = "Scegli il progetto del ToDo:"
-    let projectNameSelect = document.createElement("select");
-    projectNameLabel.setAttribute("for", "projectName");
-    projectNameSelect.setAttribute("id", "projectName");
-    projectNameSelect.setAttribute("name", "projectName");
+    function projectNameSelectForm() {
+        let projectNameLabel = document.createElement("label");
+        projectNameLabel.textContent = "Scegli il progetto del ToDo:"
+        let projectNameSelect = document.createElement("select");
+        projectNameLabel.setAttribute("for", "projectName");
+        projectNameSelect.setAttribute("id", "projectName");
+        projectNameSelect.setAttribute("name", "projectName");
 
-    formStesso.appendChild(projectNameLabel);
-    formStesso.appendChild(projectNameSelect);
+        formStesso.appendChild(projectNameLabel);
+        formStesso.appendChild(projectNameSelect);
+    }
+    
+    projectNameSelectForm();
 
     // PROJECTNAME Option loop
     function projectNameArrayOption() {
@@ -175,12 +206,21 @@ function addFormDOM(projectNameArray) {
     });
     
     // Submit Button
-    let submitButton = document.createElement("button");
-    submitButton.setAttribute("id", "submitButton");
-    submitButton.setAttribute("type", "submit");
-    submitButton.textContent = "Salva";
+    function submitButtonForm() {
+        let submitButton = document.createElement("button");
+        submitButton.setAttribute("id", "submitButton");
+        submitButton.setAttribute("type", "submit");
+        submitButton.textContent = "Salva";
 
-    formStesso.appendChild(submitButton);
+        formStesso.appendChild(submitButton);
+
+        submitButton.addEventListener("click", () => {
+            getFormValue(ToDo);
+            //projectNameArrayLength();
+        });
+    }
+
+    submitButtonForm();
 
     function addShowFormButton() {
         let formContainer = document.querySelector("#form-container");
@@ -192,10 +232,7 @@ function addFormDOM(projectNameArray) {
     }
     
     // Get all form values by calling getFormValue()
-    submitButton.addEventListener("click", () => {
-        getFormValue(ToDo);
-        //projectNameArrayLength();
-    });
+    
 
 }
 
